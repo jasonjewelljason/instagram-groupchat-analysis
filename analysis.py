3b879b3d31bc9554f9c598b0b23593c9346b0e84
jasonjewelljason <125612313+jasonjewelljason@users.noreply.github.com>
2023-11-16 11:51:07 -0600

    made author stats more succinct

--- analysis.py
+++ analysis.py
@@ -14,38 +14,28 @@ def generate_author_stats(m,l) -> pd.DataFrame:
 
     # Initializing the dataframe with unique authors
     author_stats = pd.DataFrame(m['author'].unique(), columns=['author'])
+    cols = {}
 
     # Total Sends: Counting the number of sends (rows) for each author in m
-    total_sends = m['author'].value_counts()
+    cols['total_sends'] = m['author'].value_counts()
 
     # Total Messages, Links, Images: Counting based on the 'meta' column
-    total_messages = m[m['meta'] == 'message']['author'].value_counts()
-    total_links = m[m['meta'] == 'link']['author'].value_counts()
-    total_images = m[m['meta'] == 'image']['author'].value_counts()
-    total_posts = m[m['meta'] == 'post']['author'].value_counts()
-    total_videos = m[m['meta'] == 'video']['author'].value_counts()
-    total_audios = m[m['meta'] == 'audio']['author'].value_counts()
+    for meta in ['message', 'link', 'image', 'post', 'video', 'audio']:
+        cols[f'total_{meta}s'] = m[m['meta'] == meta]['author'].value_counts()
 
     # Likes Given: Counting the number of likes given by each author in l
-    likes_given = l['liker'].value_counts()
+    cols['likes_given'] = l['liker'].value_counts()
 
     # Likes Received: Calculating the number of likes received for each author's posts
     # Mapping post_ids in m to their corresponding likes in l
     post_likes = l['post_id'].value_counts()
     m['likes_received'] = m['post_id'].map(post_likes).fillna(0)
-    likes_received = m.groupby('author')['likes_received'].sum()
+    cols['likes_received'] = m.groupby('author')['likes_received'].sum()
 
     # Merging all these counts into the author_stats dataframe
     author_stats = author_stats.set_index('author')
-    author_stats['total_sends'] = author_stats.index.map(total_sends)
-    author_stats['total_messages'] = author_stats.index.map(total_messages)
-    author_stats['total_links'] = author_stats.index.map(total_links)
-    author_stats['total_images'] = author_stats.index.map(total_images)
-    author_stats['total_posts'] = author_stats.index.map(total_posts)
-    author_stats['total_videos'] = author_stats.index.map(total_videos)
-    author_stats['total_audios'] = author_stats.index.map(total_audios)
-    author_stats['likes_given'] = author_stats.index.map(likes_given)
-    author_stats['likes_received'] = author_stats.index.map(likes_received)
+    for k,v in cols.items():
+        author_stats[k] = author_stats.index.map(v)
 
     # Filling NaN values with 0 as they indicate no activity in that category
     author_stats.fillna(0, inplace=True)
@@ -56,4 +46,4 @@ def generate_author_stats(m,l) -> pd.DataFrame:
     return author_stats
 
 if __name__ == '__main__':
-    pass
+    a = generate_author_stats(m,l)
